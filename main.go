be35fb9de912ff4bff3d249b2b7c1c3cf4173f0b
ian5258 <ian56897@icloud.com>
2023-02-19 11:11:05 +0800

    [modify] tidy code

--- main.go
+++ main.go
@@ -14,13 +14,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// var lr = &model.LogRecord{
-// 	JobName: "撿破爛1",
-// 	Command: "推車撿破爛1",
-// 	Err:     "撿不到破爛1",
-// 	Content: "剪了好多破爛1",
-// }
-
 func main() {
 	lr := new(model.LogRecord)
 	lr.JobName = "撿破爛5566"
@@ -28,18 +21,16 @@ func main() {
 	lr.Err = "撿不到破爛5566"
 	lr.Content = "剪了好多破爛5566"
 
-	// lr := &model.LogRecord{
-	// 	JobName: "撿破爛",
-	// 	Command: "推車撿破爛",
-	// 	Err:     "撿不到破爛",
-	// 	Content: "剪了好多破爛",
-	// }
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
+	// Connect
 	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	collection := client.Database("first-project").Collection("todoList")
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	// Insert
 	collection.InsertOne(context.TODO(), lr)
 
 	// GET
@@ -49,25 +40,9 @@ func main() {
 	}
 	defer cur.Close(ctx)
 	var results []model.LogRecord
-	// 使用All函数获取所有查询结果，并将结果保存至results变量。
 	if err = cur.All(context.TODO(), &results); err != nil {
 		log.Fatal(err)
 	}
 	jsondata, _ := json.Marshal(results)
 	fmt.Println(string(jsondata))
-	// fmt.Println(results)
-	// 遍历结果数组
-	// for _, result := range results {
-	// 	fmt.Println(*result)
-	// }
-
-	// for cur.Next(ctx) {
-	// 	var document bson.M
-	// 	err = cur.Decode(&document)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 	}
-	// 	results = append(results, document)
-	// }
-	// fmt.Println(results)
 }
